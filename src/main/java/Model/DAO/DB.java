107829a84cc1d1833ebd7b2144a4a8edaa76f9f2
amy <ctrlalternativedelete@gmail.com>
2025-04-10 17:34:47 +1000

    fix: DAO configuration

--- src/main/java/Model/DAO/DB.java
+++ src/main/java/Model/DAO/DB.java
@@ -7,8 +7,8 @@ import java.sql.Connection;
 *  
 */
 public abstract class DB {   
-    protected String URL = "jdbc:sqlite:";
-    protected String db = "store.db";//name of the database   
+    protected String URL = "jdbc:sqlite:store.db";
+    protected String db = "";//name of the database   
     protected String dbuser = "";//db root user   
     protected String dbpass = ""; //db root password   
     protected Connection conn; //connection null-instance to be initialized in sub-classes
